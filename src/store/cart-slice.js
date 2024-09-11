225f4f4839e5b1fd4ac2ad02ff966735d2240f06
ms-tech-geek <thecorporator@gmail.com>
2024-09-12 01:43:15 +0530

    add item to cart

---
+++ src/store/cart-slice.js
@@ -0,0 +1,31 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialCartState = {
+	items: [],
+	totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+	name: "cart",
+	initialState: initialCartState,
+	reducers: {
+		addItemToCart(state, action) {
+			const newItem = action.payload;
+			const existingItem = state.items.find((item) => item.id === newItem.id);
+
+			if (!existingItem) {
+				state.items.push({
+					itemId: newItem.id,
+					price: newItem.price,
+					quantity: 1,
+					totalPrice: newItem.price,
+					name: newItem.title,
+				});
+			} else {
+				existingItem.quantity = existingItem.quantity++;
+				existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+			}
+		},
+		removeItemFromCart() {},
+	},
+});
