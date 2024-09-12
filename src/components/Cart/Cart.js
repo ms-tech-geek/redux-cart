0b523846bc0656efcb37e9d3f6a618559c09f721
ms-tech-geek <thecorporator@gmail.com>
2024-09-12 12:39:35 +0530

    clean up

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,6 @@ import CartItem from "./CartItem";
 
 const Cart = () => {
 	const cartItems = useSelector((state) => state.cart.items);
-  console.log(cartItems)
 	return (
 		<Card className={classes.cart}>
 			<h2>Your Shopping Cart</h2>
