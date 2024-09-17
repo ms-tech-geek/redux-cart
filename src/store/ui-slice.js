8c02226a716c456636a4a154466e812c0bc203e5
ms-tech-geek <thecorporator@gmail.com>
2024-09-17 11:57:44 +0530

    code cleanup

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,12 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUIState = {
-	cartIsVisible: false,
-};
-
 const uiSlice = createSlice({
 	name: "ui",
-	initialState: initialUIState,
+	initialState: { cartIsVisible: false },
 	reducers: {
 		toggle(state) {
 			state.cartIsVisible = !state.cartIsVisible;
